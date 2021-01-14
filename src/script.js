c3ae8b4fd84a67d5a1640150c02d5d46d0469356
Marcus Luebke <marcusxluebke@gmail.com>
2021-01-14 00:59:20 -0800

    Everything works except modules now
    gamer time
    thanks corwin

--- src/script.js
+++ src/script.js
@@ -4,21 +4,26 @@ const path = _require('path');
 const { dialog, BrowserWindow, nativeImage } = electron.remote;
 
 let calculator = null;
-let package = {
+let module = {
 	config: {
-		isPackage: false,
-		letter: null,
+		isModule: false,
+		moduleLetter: null,
 		get name() {
 			return document.getElementById("ds-name").value;
 		}
 	},
-	previousState: null,
-	isSaved: true,
+	saveState: null,
+	isSaved() {
+		return module.saveState == getSaveState();
+	},
 	fileName: null,
 	filePath: null,
 	fileDirectory: electron.remote.app.getPath("documents"),
 	getDefaultFileName() {
-		return package.config.name.replace(/[^0-9a-z]+/gi,"-") + ".dsms";
+		return module.config.name.replace(/[^0-9a-z]+/gi,"-") + ".dsms";
+	},
+	setName(name) {
+		document.getElementById("ds-name").value = name;
 	}
 };
 
@@ -26,8 +31,8 @@ function onkeydown(evt) {
 	if(evt.ctrlKey && !evt.shiftKey && !evt.altKey) {
 		if(evt.key == "s") save();
 		if(evt.key == "o") openFile();
-		if(evt.key == "i") importPackage();
-		if(evt.key == "n") newPackage();
+		if(evt.key == "i") importModule();
+		if(evt.key == "n") newModule();
 	}
 }
 
@@ -54,31 +59,43 @@ function createFolder(title, obj) {
 	
 // }
 
-function getStateFromFilePath(filePath) {
+function getSaveObjFromFilePath(filePath) {
 	return JSON.parse(fs.readFileSync(filePath).toString());
 }
 
 async function warnSave() {
-	const thisWindow = BrowserWindow.getFocusedWindow();
-	let result = await dialog.showMessageBox(thisWindow, {
-		title: "Warning - Unsaved work",
-		buttons: ["Go Back", "Continue"],
-		message: "You have unsaved work. Do you want to continue, which will erase unsaved changes?"
-	});
-	return {willCancel: result == 0};
+	let willCancel = false;
+	if(!module.isSaved()) {
+		const thisWindow = BrowserWindow.getFocusedWindow();
+		let result = await dialog.showMessageBox(thisWindow, {
+			title: "Warning - Unsaved work",
+			buttons: ["Go Back", "Continue"],
+			message: "You have unsaved work. Do you want to continue, which will erase unsaved changes?"
+		});
+		console.log(result);
+		willCancel = result == 0;
+	}
+	return { willCancel };
 }
 
 async function openFile() {
-	if(!package.isSaved) {
+	if(!module.isSaved) {
 		if((await warnSave()).willCancel) return;
 	}
 	let result = await dialog.showOpenDialog({ properties: ['openFile'] });
 	console.log(result);
 	if(result.canceled) return;
-	calculator.setState(getStateFromFilePath(result.filePaths[0]));
+	let saveObj = getSaveObjFromFilePath(result.filePaths[0]);
+	let config = saveObj.config;
+	module.setName(config.name);
+	setIsModule(config.isModule);
+	if(config.isModule) {
+		declareModule(config.moduleLetter)
+	}
+	calculator.setState(saveObj.state);
 }
 
-function importPackage() {
+function importModule() {
 	
 	
 	let state = calculator.getState();
@@ -96,52 +113,57 @@ function importPackage() {
 	calculator.setState(state);
 }
 
-function newPackage() {
-	
+function declareModule(moduleLetter) {
+	module.config.isModule = true;
+	module.config.moduleLetter = moduleLetter;
 }
 
-function clear() {
-	
+function setIsModule(isModule) {
+	document.getElementById("ds-is-module").checked = isModule;
+	module.config.isModule = isModule;
+	document.getElementById("ds-module-letter-container").hidden = !isModule;
+	document.getElementById("ds-project-or-module").innerHTML = isModule ? "Module" : "Project";
 }
 
 function getSaveState() {
 	let state = calculator.getState();
-	return JSON.stringify({
+	let saveObj = {
 		state,
-		config: package.config
-	});
+		config: module.config
+	};
+	return JSON.stringify(saveObj);
 }
 
 async function _save() {
-	package.saveState = getSaveState();
-	await fs.writeFile(package.filePath, package.saveState, () => {});
+	module.saveState = getSaveState();
+	await fs.writeFile(module.filePath, module.saveState, () => {});
 }
 
 async function saveAs(){
-	if(package.filePath == null) {
-		package.filePath = await path.resolve(package.fileDirectory, package.getDefaultFileName());
+	if(module.filePath == null) {
+		module.filePath = await path.resolve(module.fileDirectory, module.getDefaultFileName());
 	}
 	console.log()
-	let userChosenPath = await dialog.showSaveDialog({ defaultPath: package.filePath });
+	let userChosenPath = await dialog.showSaveDialog({ defaultPath: module.filePath });
 	if(!userChosenPath.canceled){
-		package.filePath = userChosenPath.filePath;
-		package.fileDirectory = path.dirname(package.filePath);
-		package.fileName = path.basename(package.filePath);
+		module.filePath = userChosenPath.filePath;
+		module.fileDirectory = path.dirname(module.filePath);
+		module.fileName = path.basename(module.filePath);
 		await _save();
 	} else {
-		package.filePath = null;
+		module.filePath = null;
 	}
 }
 
 async function save() {
-	if(package.filePath == null) await saveAs();
+	if(module.filePath == null) await saveAs();
 	else await _save();
 }
 
 function onload() {
 	document.body.addEventListener("keydown", onkeydown);
-	
 	let elem = document.getElementById("calculator");
+	elem.innerHTML = "";
 	calculator = Desmos.Calculator(elem);
-	package.savedState = JSON.stringify(calculator.getState());
+	module.saveState = JSON.stringify(getSaveState());
 }
