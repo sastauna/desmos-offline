18331d6d3f0be84d36bd0949c95a7a07dacb8102
Marcus Luebke <marcusxluebke@gmail.com>
2021-01-14 00:08:17 -0800

    Finished save mechanism
    Everything seems to be coming along nicely

--- src/script.js
+++ src/script.js
@@ -1,22 +1,24 @@
-var electron = _require('electron');
-var fs = _require('fs');
-var path = _require('path');
-
-
+const electron = _require('electron');
+const fs = _require('fs');
+const path = _require('path');
+const { dialog, BrowserWindow, nativeImage } = electron.remote;
 
 let calculator = null;
 let package = {
-	appendPackagesToFront: false,
-	isPackage: false,
-	letter: null,
+	config: {
+		isPackage: false,
+		letter: null,
+		get name() {
+			return document.getElementById("ds-name").value;
+		}
+	},
+	previousState: null,
+	isSaved: true,
 	fileName: null,
 	filePath: null,
 	fileDirectory: electron.remote.app.getPath("documents"),
-	getPackageName() {
-		return document.getElementById("ds-name").value
-	},
 	getDefaultFileName() {
-		return package.getPackageName().replace(/[^0-9a-z]+/gi,"-") + ".dsms";
+		return package.config.name.replace(/[^0-9a-z]+/gi,"-") + ".dsms";
 	}
 };
 
@@ -45,8 +47,6 @@ function createFolder(title, obj) {
 	calculator.setState(state);
 }
 
-// function moveExpression()
-
 // function getNextId() {
 // 	return calculator.getState().
 // }
@@ -54,13 +54,46 @@ function createFolder(title, obj) {
 	
 // }
 
+function getStateFromFilePath(filePath) {
+	return JSON.parse(fs.readFileSync(filePath).toString());
+}
 
-function openFile() {
-	
+async function warnSave() {
+	const thisWindow = BrowserWindow.getFocusedWindow();
+	let result = await dialog.showMessageBox(thisWindow, {
+		title: "Warning - Unsaved work",
+		buttons: ["Go Back", "Continue"],
+		message: "You have unsaved work. Do you want to continue, which will erase unsaved changes?"
+	});
+	return {willCancel: result == 0};
+}
+
+async function openFile() {
+	if(!package.isSaved) {
+		if((await warnSave()).willCancel) return;
+	}
+	let result = await dialog.showOpenDialog({ properties: ['openFile'] });
+	console.log(result);
+	if(result.canceled) return;
+	calculator.setState(getStateFromFilePath(result.filePaths[0]));
 }
 
 function importPackage() {
 	
+	
+	let state = calculator.getState();
+	let exps = state.expressions.list;
+	let id = (+exps[exps.length - 1].id + 1).toString();
+	exps.push(Object.assign({
+		id,
+		title,
+		type: "folder",
+		memberIds: {},
+		hidden: true,
+		collapsed: true,
+		secret: false,
+	}, obj));
+	calculator.setState(state);
 }
 
 function newPackage() {
@@ -71,37 +104,44 @@ function clear() {
 	
 }
 
+function getSaveState() {
+	let state = calculator.getState();
+	return JSON.stringify({
+		state,
+		config: package.config
+	});
+}
+
 async function _save() {
-	await fs.writeFile(package.filePath, calculator.getOutput(), () => {});
+	package.saveState = getSaveState();
+	await fs.writeFile(package.filePath, package.saveState, () => {});
+}
+
+async function saveAs(){
+	if(package.filePath == null) {
+		package.filePath = await path.resolve(package.fileDirectory, package.getDefaultFileName());
+	}
+	console.log()
+	let userChosenPath = await dialog.showSaveDialog({ defaultPath: package.filePath });
+	if(!userChosenPath.canceled){
+		package.filePath = userChosenPath.filePath;
+		package.fileDirectory = path.dirname(package.filePath);
+		package.fileName = path.basename(package.filePath);
+		await _save();
+	} else {
+		package.filePath = null;
+	}
 }
 
 async function save() {
-	if(!calculator.fileName) await saveAs();
+	if(package.filePath == null) await saveAs();
 	else await _save();
 }
-
 
 function onload() {
 	document.body.addEventListener("keydown", onkeydown);
 	
 	let elem = document.getElementById("calculator");
 	calculator = Desmos.Calculator(elem);
-	calculator.createFolder = name => {
-		
-	};
-}
-
-
-
-async function saveAs(){
-	if(package.filePath == null) {
-		package.filePath = await path.resolve(package.fileDirectory, package.getDefaultFileName());
-	}
-	var userChosenPath = await electron.remote.dialog.showSaveDialog({ defaultPath: package.fileDirectory });
-	if(userChosenPath){
-		package.filePath = userChosenPath.filePath;
-		package.fileDirectory = path.dirname(package.filePath);
-		package.fileName = path.basename(package.filePath);
-		await _save();
-	}
+	package.savedState = JSON.stringify(calculator.getState());
 }
